cc2150121a33c0f6263687e19644866bddaa6c1e
Zlobin Anton <antony.z@mail.ru>
2023-05-11 22:46:52 +0300

    Added component for the card of the item

--- src/components/itemCard.jsx
+++ src/components/itemCard.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Button from "./button";
 
-function ItemCard () {
+function ItemCard (props) {
+  let buyButton = {text: "Купить"}
+  let inCartButton = {text: "Добавить в корзину"}
   return (
-    <div>
-
+    <div className="itemCard">
+      <img/>
+      <p className="itemName">{props.tire.name}</p>
+      <p className="brand">{props.tire.brand}</p>
+      <table>
+        <tr>
+          <td className="characteristic">Ширина, мм</td>
+          <td>{props.tire.width}</td>
+        </tr>
+        <tr>
+          <td className="characteristic">Профиль, %</td>
+          <td>{props.tire.profile}</td>
+        </tr>
+        <tr>
+          <td className="characteristic">Диаметр, дюйм</td>
+          <td>{props.tire.diameter}</td>
+        </tr>
+        <tr>
+          <td className="characteristic">Тип автомобиля</td>
+          <td>{props.tire.carType}</td>
+        </tr>
+        <tr>
+          <td className="characteristic">Сезон</td>
+          <td>{props.tire.season}</td>
+        </tr>
+        <tr>
+          <td className="characteristic">Шипы</td>
+          <td>{props.tire.spikes}</td>
+        </tr>
+      </table>
+      <p className="cost">{props.tire.cost} руб.</p>
+      <div className="buttonContainer">
+        <Button button={buyButton}/>
+        <Button button={inCartButton}/>
+      </div>
     </div>
   )
 }
+export default ItemCard;
--- src/pages/catalog.jsx
+++ src/pages/catalog.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import ItemCard from "../components/itemCard";
 
 function Catalog () {
+  let cordSum185 = {
+    name: "Шина летняя 185/65R15 ROAD_RUNNER, PS-1 б/к",
+    brand: "CORDIANT",
+    width: 185,
+    profile: 65,
+    diameter: 15,
+    carType: "Легковой",
+    season: "Летняя",
+    spikes: "Нет",
+    cost: 3674
+  }
   return (
-    <div className="content-container">Catalog!</div>
+    <div className="content-container">
+      <h1>Каталог</h1>
+      <div className="itemsContainer">
+        <ItemCard tire={cordSum185}/>
+        <ItemCard tire={cordSum185} />
+      </div>
+    </div>
   )
 }
 export default Catalog;
