ba14dd22399331e226108b94b19a74a211cc5f98
Zlobin Anton <antony.z@mail.ru>
2023-05-31 14:33:41 +0300

    Started devepoping backend

--- src/frontend/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-function Footer() {
-  return (
-    <footer>
-      © Zlobin Anton, RTU MIREA 2023
-    </footer>
-  )
-}
-export default Footer;
--- src/frontend/pages/personalAccount.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function PersonalAccount () {
-  return (
-    <p>Личный кабинет</p>
-  )
-}
-export default PersonalAccount;
